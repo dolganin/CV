074dbcddedbddb790623aa6c332438b388702002
dolganin <s.bespalov1@g.nsu.ru>
2023-03-08 23:02:59 +0700

    Trying to preprocess data

---
+++ functions.py
@@ -0,0 +1,28 @@
+import torch
+import matplotlib.pyplot as plt
+from os import listdir
+from cv2 import imread
+from cv2 import dnn
+
+
+rootdir = "simpsons_dataset"
+
+
+def grad(func):
+    return None
+
+
+def get_persons_list():
+    return listdir(rootdir)
+
+def read_train_set():
+    dataset=dict()
+    for directory in listdir(rootdir):
+        lst_1_person = []
+        for image in listdir(rootdir+'\\'+directory):
+            rawimage = imread(rootdir+'\\'+directory+'\\'+image)
+            blob = dnn.blobFromImage(rawimage, 1/255.0, (28,28), swapRB = True, crop = False)
+            lst_1_person.append(rawimage)
+        dataset[directory] = lst_1_person
+    return dataset
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from functions import read_train_set
+from functions import get_persons_list
+
+
+persons_list = get_persons_list()
+
+data = read_train_set()
+
