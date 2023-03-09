cc8628bd9babc4fea85b1318ec3e2c8c4b7a7d32
dolganin <s.bespalov1@g.nsu.ru>
2023-03-09 09:02:45 +0700

    This is working for 1.5 munites...

--- functions.py
+++ functions.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plt
 from os import listdir
 from cv2 import imread
 from cv2 import dnn
-
+from cv2 import cvtColor
+from cv2 import COLOR_BGR2GRAY
+from tensorflow import keras
+import tensorflow as tf
+import numpy as np
 
 rootdir = "simpsons_dataset"
-
 
 def grad(func):
     return None
@@ -20,14 +23,21 @@ def get_persons_list():
     return pers_dict
 
 
-def read_train_set():
-    dataset=dict()
+def read_full_set():
+    dataset=[]
     for directory in listdir(rootdir):
-        lst_1_person = []
+        lst_1_person = np.zeros((len(listdir(rootdir+'\\'+directory)), 3, 28, 28))
+        i = 0
         for image in listdir(rootdir+'\\'+directory):
             rawimage = imread(rootdir+'\\'+directory+'\\'+image)
+            rawimage = cvtColor(rawimage, COLOR_BGR2GRAY)
             blob = dnn.blobFromImage(rawimage, 1/255.0, (28,28), swapRB = True, crop = False)
-            lst_1_person.append(rawimage)
-        dataset[directory] = lst_1_person
+            blob = blob.reshape(1,28,28)
+            lst_1_person[i] = blob
+            i += 1
+        dataset.append(lst_1_person)
     return dataset
+
+
+
 
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from functions import read_train_set
+from functions import read_full_set
 from functions import get_persons_list
+import matplotlib.pyplot as plt
 
 
 persons_list = get_persons_list()
 
+data = read_full_set()
 
-data = read_train_set()
+plt.figure(figsize=(10,10))
+for i in range(25):
+ plt.subplot(5,5,i+1)
+ plt.xticks([])
+ plt.yticks([])
+ plt.grid(False)
+ plt.imshow(data[0][i], cmap=plt.cm.binary)
+plt.show()
+
 
