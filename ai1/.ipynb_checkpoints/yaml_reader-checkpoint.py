65cd59972b08698aea0f44a21d3aef0f40c8c2cd
Долганин Сергей <102363471+dolganin@users.noreply.github.com>
2024-02-05 19:24:04 +0700

    Delete ai1 directory

--- ai1/.ipynb_checkpoints/yaml_reader-checkpoint.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-def yaml_reader():
-    with open("config.yaml","r") as file:
-        config = yaml.safe_load(file)
-    return config
--- ai1/focused_loss.py
+++
@@ -1,4 +0,0 @@
-from imports import *
-
-def focused_loss():
-    return torch.Tensor([1-(len(listdir(join(rootdir, el))))/counter for el in classlist]).to(device)
